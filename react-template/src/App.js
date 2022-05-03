59bb0d534cdc1f5012931d57fd0276a2129a6286
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2022-05-03 03:23:23 -0400

    Copyright Section added

--- react-template/src/App.js
+++ react-template/src/App.js
@@ -4,6 +4,8 @@ import MastHeadSection from "./MastHeadSection";
 import PortfolioSection from "./PortfolioSection";
 import AboutSection from "./AboutSection";
 import ContactSection from "./ContactSection";
+import FooterSection from "./FooterSection";
+import CopyrightSection from "./CopyrightSection";
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
       <PortfolioSection />
       <AboutSection />
       <ContactSection />
+      <FooterSection />
+      <CopyrightSection />
     </div>
   );
 };
